2099207eeda1b2ffdf1970c0fbf0ac68438eb962
pragya234 <pandeypragya026@gmail.com>
2020-10-27 19:43:39 +0530

    staged 2 files for commit

---
+++ IfElse1.java
@@ -0,0 +1,11 @@
+public class IfElse1{
+public static void main(String args[]){
+int x=-5;
+if(x>0){
+System.out.println("x is positive");
+}
+else{
+System.out.println("x is negative");
+}
+}
+}
---
+++ IfElse2.java
@@ -0,0 +1,11 @@
+public class IfElse2{
+public static void main(String args[]){
+int x=89;
+if(x%2==0){
+System.out.println("x is an even number");
+}
+else{
+System.out.println("x is a odd number");
+}
+}
+}
