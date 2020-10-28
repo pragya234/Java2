2ef8c238e1f4c622bcffa675a1084ad4aaf8f4b3
pragya234 <pandeypragya026@gmail.com>
2020-10-29 00:46:18 +0530

    written even number and odd number program

---
+++ Even.java
@@ -0,0 +1,10 @@
+public class Even{
+public static void main(String args[]){
+int x = 57;
+if(x%2==0){
+System.out.println("x is an even number");
+}else{
+System.out.println("x is a odd number");
+}
+}
+}
---
+++ Odd.java
@@ -0,0 +1,10 @@
+public class Odd{
+public static void main(String args[]){
+int x=79;
+if(x%2==0){
+System.out.println("not an odd number");
+}else{
+System.out.println("an odd number");
+}
+}
+}
