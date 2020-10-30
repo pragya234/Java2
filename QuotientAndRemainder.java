69b79b24d5ea64e08887c2be3a60fad71b73daa5
pragya234 <pandeypragya026@gmail.com>
2020-10-30 19:11:08 +0530

    written a program to calculate quotient and remainder dividing 2 numbers

---
+++ QuotientAndRemainder.java
@@ -0,0 +1,10 @@
+public class QuotientAndRemainder{
+public static void main(String args[]){
+int num1= 30;
+int num2= 387;
+int quo= num2/num1;
+int rem=num2%num1;;
+System.out.println("Quotient is "+quo);
+System.out.println("Remainder is "+rem);
+}
+}
