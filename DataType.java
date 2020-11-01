00411e779a347d8392c48dde667af1e43234c6e8
pragya234 <pandeypragya026@gmail.com>
2020-11-01 15:39:03 +0530

    Created a file with the same name in other branch and now trying to checkout to that branch

---
+++ DataType.java
@@ -0,0 +1,4 @@
+public class DataType{
+/*I am creating this file in main branch as file with the same name exists in java_basic branch, now when I will try to switch from main branch to java_basic branch,git will stop me from switiching because the history of file DataType.java is different in both the branch, If history would have been same we could have easily switched to java_basic branch
+*/
+}
