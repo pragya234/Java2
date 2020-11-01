5bd7423b2e42a9b9547b6fa3920282e76bd05f13
pragya234 <pandeypragya026@gmail.com>
2020-11-01 15:16:59 +0530

    written a program to clear datatype concept in java

---
+++ DataType.java
@@ -0,0 +1,18 @@
+public class DataType{
+public static void main(String args[]){
+/*
+there are two types of datatypes in java: 1. Primitive Datatype
+2. Non primitive datatype
+Definition of primitive data type: Primitive datatypes are basic datatype that are provided by language and language have a reserved keyword for all the primitive datatypes
+Examples:
+int
+char
+float
+double
+boolean
+long
+short
+byte
+*/
+}
+}
