580c0d9ab8265ca27d8f2f8e926056b2f860c6cb
pragya234 <pandeypragya026@gmail.com>
2020-11-01 15:55:33 +0530

    I learnt that if we create a new file in any branch we can switch branches and add it to any other branch but if we create a file with the same name in other branch which we have already committed then we wont be able to switch branch either we will have to commit it to that branch or delete it

---
+++ Example.java
@@ -0,0 +1,7 @@
+public class Example{
+/*terminal command: to remove file- rm file_name
+to remove multiple files - rm *.extension of the file
+git command to remove files from untracked stage- git clean -d -f
+
+*/
+}
