b621b5da438bdca7859ced13d31297d643eb0c0a
pragya234 <pandeypragya026@gmail.com>
2020-11-01 14:56:50 +0530

    written a simple interest program

---
+++ SimpleInterest.java
@@ -0,0 +1,9 @@
+public class SimpleInterest{
+public static void main(String args[]){
+int p=10000;
+float r=7.5f;
+int t=2;
+float si=(p*r*t)/100;
+System.out.println("simple interest is"+si);
+}
+}
